81beea4431526b0aa4409037fe635bcdda36e208
Kristian <fich2402@gmail.com>
2024-03-30 01:59:32 +0100

    removed credentials from wifimanager to seperate handler

--- lib/wifi_manager.py
+++ lib/wifi_manager.py
@@ -4,9 +4,9 @@ import socket
 import re
 import time
 import helpers.import_helper as ih
-
+from lib.wifi.wifi_credentials_handler import WifiCredentialsHandler
+from lib.wifi.web_page_handler import WebPageHandler
 class WifiManager:
-
     def __init__(self, ssid = 'WifiManager', password = 'wifimanager', reboot = True, debug = True):
         self.wlan_sta = network.WLAN(network.STA_IF)
         self.wlan_sta.active(True)
@@ -21,11 +21,12 @@ class WifiManager:
         self.wlan_sta.disconnect()
         self.reboot = reboot      
         self.debug = debug
-
+        self.cd_handler = WifiCredentialsHandler()
+        
     def connect(self):
         if self.wlan_sta.isconnected():
             return
-        profiles = self.read_credentials()
+        profiles = self.cd_handler.read_credentials()
         for ssid, *_ in self.wlan_sta.scan():
             ssid = ssid.decode("utf-8")
             if ssid in profiles:
@@ -45,29 +46,6 @@ class WifiManager:
 
     def get_address(self):
         return self.wlan_sta.ifconfig()
-
-    def write_credentials(self, profiles):
-        lines = []
-        for ssid, password in profiles.items():
-            lines.append('{0};{1}\n'.format(ssid, password))
-        with open(self.wifi_credentials, 'w') as file:
-            file.write(''.join(lines))
-
-    def read_credentials(self):
-        lines = []
-        try:
-            with open(self.wifi_credentials) as file:
-                lines = file.readlines()
-        except Exception as error:
-            if self.debug:
-                print(error)
-            pass
-        profiles = {}
-        for line in lines:
-            ssid, password = line.strip().split(';')
-            profiles[ssid] = password
-        return profiles
-
 
     def wifi_connect(self, ssid, password):
         connection_message = 'Connecting to: ' + ssid
@@ -115,27 +93,21 @@ class WifiManager:
                 machine.reset()
         try:
             self.client, addr = self.server_socket.accept()
-            # If we reach here, a client has connected successfully
-            # Proceed with handling the client
+            wp_handler = WebPageHandler(self.client)
         except Exception as error:
-            # If an exception occurs, it's likely because no client is connecting
-            # Log the error if debug is enabled, but don't raise it further
             if self.debug:
                 print("No client connection available:", error)
-            return  # Simply return to allow for other operations, like checking for button presses
-
+            return
         try:
             self.client.settimeout(5.0)
             self.request = b''
             try:
                 while True:
                     if '\r\n\r\n' in self.request:
-                        # Fix for Safari browser
                         self.request += self.client.recv(512)
                         break
                     self.request += self.client.recv(128)
             except Exception as error:
-                # It's normal to receive timeout errors in this stage, we can safely ignore them.
                 if self.debug:
                     print(error)
                 pass
@@ -144,7 +116,7 @@ class WifiManager:
                     print(self.url_decode(self.request))
                 url = re.search('(?:GET|POST) /(.*?)(?:\\?.*?)? HTTP', self.request).group(1).decode('utf-8').rstrip('/')
                 if url == '':
-                    self.handle_root()
+                    wp_handler.handle_root(self.wlan_sta, self.ap_ssid)
                 elif url == 'configure':
                     self.handle_configure()
                 else:
@@ -155,13 +127,11 @@ class WifiManager:
             return
         finally:
             self.client.close()
-    
 
     def send_header(self, status_code = 200):
         self.client.send("""HTTP/1.1 {0} OK\r\n""".format(status_code))
         self.client.send("""Content-Type: text/html\r\n""")
         self.client.send("""Connection: close\r\n""")
-
 
     def send_response(self, payload, status_code = 200):
         self.send_header(status_code)
@@ -180,7 +150,6 @@ class WifiManager:
             </html>
         """.format(payload))
         self.client.close()
-
 
     def handle_root(self):
         self.send_header()
@@ -211,7 +180,6 @@ class WifiManager:
         """)
         self.client.close()
 
-
     def handle_configure(self):
         match = re.search('ssid=([^&]*)&password=(.*)', self.url_decode(self.request))
         if match:
@@ -228,9 +196,9 @@ class WifiManager:
                     <h1>{0}</h1>
                     <p>IP address: {1}</p>
                 """.format(ssid, self.wlan_sta.ifconfig()[0]))
-                profiles = self.read_credentials()
+                profiles = self.cd_handler.read_credentials()
                 profiles[ssid] = password
-                self.write_credentials(profiles)
+                self.cd_handler.write_credentials(profiles)
                 time.sleep(5)
             else:
                 self.send_response("""
@@ -245,29 +213,22 @@ class WifiManager:
             """, 400)
             time.sleep(5)
 
-
     def handle_not_found(self):
         self.send_response("""
             <p>Page not found!</p>
         """, 404)
 
-
     def url_decode(self, url_string):
         if not url_string:
             return b''
-
         if isinstance(url_string, str):
             url_string = url_string.encode('utf-8')
-
         bits = url_string.split(b'%')
-
         if len(bits) == 1:
             return url_string
-
         res = [bits[0]]
         appnd = res.append
         hextobyte_cache = {}
-
         for item in bits[1:]:
             try:
                 code = item[:2]
@@ -281,6 +242,5 @@ class WifiManager:
                     print(error)
                 appnd(b'%')
                 appnd(item)
-
         return b''.join(res)
 
